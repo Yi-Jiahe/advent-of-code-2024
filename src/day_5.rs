7fc54b2c75372e0f14db60e3a365c1ace53479c0
Jiahe <yi.jiahe00@gmail.com>
2024-12-06 19:44:37 +0800

    day 5 part 1

---
+++ src/day_5.rs
@@ -0,0 +1,73 @@
+use std::collections::{HashMap, HashSet};
+
+pub fn part_1(input: &str) -> String {
+    let (page_ordering_rules, updates) = parse_input(input);
+
+    let mut ans = 0;
+    for line in updates.lines() {
+        let pages = line
+            .split(',')
+            .map(|s| s.trim().parse::<i32>().unwrap())
+            .collect::<Vec<i32>>();
+
+        // Pages which need to be printed regardless of order
+        let update_pages: HashSet<i32> = pages.iter().cloned().collect();
+
+        let mut right_order = true;
+        let mut printed_pages = HashSet::new();
+        for page in &pages {
+            // Identify if the current page has any rules
+            if let Some(rule_required_pages) = page_ordering_rules.get(&page) {
+                // Identify which pages are required based on the pages to be printed
+                let required_pages = update_pages
+                    .intersection(rule_required_pages)
+                    .cloned()
+                    .collect::<HashSet<i32>>();
+                // Only the subset from the rules which also need to be printed need to be checked against
+                if !required_pages.is_subset(&printed_pages) {
+                    right_order = false;
+                    break;
+                }
+            }
+
+            printed_pages.insert(*page);
+        }
+
+        if right_order {
+            // Assume that there is always an odd number of pages for there to be a middle page
+            ans += pages[(pages.len() - 1) / 2];
+        }
+    }
+
+    ans.to_string()
+}
+
+pub fn part_2(input: &str) -> String {
+    todo!();
+}
+
+// page_ordering_rules is a map describing the pages that must be printed before a given page
+// The set of required pages includes pages which might not need to be printed
+fn parse_input(input: &str) -> (HashMap<i32, HashSet<i32>>, String) {
+    let normalized_input = input.replace("\r\n", "\n");
+
+    let sections = normalized_input.split("\n\n").collect::<Vec<&str>>();
+
+    let page_ordering_rules_section = sections[0];
+    let updates_section = sections[1].to_string();
+
+    let mut page_ordering_rules: HashMap<i32, HashSet<i32>> = HashMap::new();
+    for line in page_ordering_rules_section.lines() {
+        let values = line.split_once('|').unwrap();
+        let x = values.0.trim().parse::<i32>().unwrap();
+        let y = values.1.trim().parse::<i32>().unwrap();
+
+        if page_ordering_rules.contains_key(&y) {
+            page_ordering_rules.get_mut(&y).unwrap().insert(x);
+        } else {
+            page_ordering_rules.insert(y, HashSet::from([x]));
+        }
+    }
+
+    (page_ordering_rules, updates_section)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
 use std::{env, fs, path::Path};
 
-use advent_of_code_2024::{day_1, day_2, day_3, day_4};
+use advent_of_code_2024::{day_1, day_2, day_3, day_4, day_5};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(&args);
 
     let day = &args[1];
 
     let path = Path::new(".").join("input").join(format!("day_{}.txt", day));
-    dbg!(&path);
 
     let input = fs::read_to_string(path).unwrap();
-    // dbg!(&input);
 
     match day.as_str() {
         "1" => {
@@ -41,6 +38,13 @@ fn main() {
             println!("Part 1: {}", ans_part_1);
 
             let ans_part_2 = day_4::part_2(&input);
+            println!("Part 2: {}", ans_part_2);
+        }
+        "5" => {
+            let ans_part_1 = day_5::part_1(&input);
+            println!("Part 1: {}", ans_part_1);
+
+            let ans_part_2 = day_5::part_2(&input);
             println!("Part 2: {}", ans_part_2);
         }
         _ => todo!(),

c3ee05da9215ffd6703f0b6da13c35b1cccf0d9f
Jiahe <yi.jiahe00@gmail.com>
2024-12-07 21:28:25 +0800

    day 7 part 2

--- src/day_7.rs
+++ src/day_7.rs
@@ -11,7 +11,7 @@ pub fn part_1(input: &str) -> String {
             .map(|s| s.parse::<i64>().unwrap())
             .collect::<Vec<i64>>();
 
-        if place_operator(test_value, numbers[0], 0, &numbers) {
+        if place_operator(test_value, numbers[0], 0, &numbers, 1) {
             total_calibration_result += test_value;
         }
     }
@@ -20,14 +20,38 @@ pub fn part_1(input: &str) -> String {
 }
 
 pub fn part_2(input: &str) -> String {
-    todo!()
+  let mut total_calibration_result = 0;
+
+  for line in input.lines() {
+      let values = line.split_once(':').unwrap();
+      let test_value = values.0.trim().parse::<i64>().unwrap();
+      let numbers = values
+          .1
+          .trim()
+          .split_whitespace()
+          .map(|s| s.parse::<i64>().unwrap())
+          .collect::<Vec<i64>>();
+
+      if place_operator(test_value, numbers[0], 0, &numbers, 2) {
+          total_calibration_result += test_value;
+      }
+  }
+
+  total_calibration_result.to_string()
 }
 
-fn place_operator(test_value: i64, current_value: i64, i: usize, numbers: &Vec<i64>) -> bool {
+fn place_operator(test_value: i64, current_value: i64, i: usize, numbers: &Vec<i64>, part: i32) -> bool {
     if i == numbers.len() - 1 {
         return current_value == test_value;
     }
 
-    return place_operator(test_value, current_value + numbers[i + 1], i + 1, numbers)
-        || place_operator(test_value, current_value * numbers[i + 1], i + 1, numbers);
+    let part_1_check = place_operator(test_value, current_value + numbers[i + 1], i + 1, numbers, part)
+        || place_operator(test_value, current_value * numbers[i + 1], i + 1, numbers, part);
+      
+    if part == 1 {
+        return part_1_check;
+    }
+
+    return part_1_check
+        || place_operator(test_value, format!("{}{}", current_value.to_string(), numbers[i + 1].to_string()).parse::<i64>().unwrap(), i + 1, numbers, part);
 }
